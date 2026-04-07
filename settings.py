2460468f69f2301586d204b819bf91a1e4c51119
Akwasi Boahene <boahenkwasi569@gmail.com>
2026-04-07 16:45:38 +0000

    Contains codes to make changes

---
+++ settings.py
@@ -0,0 +1,24 @@
+class Settings:
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1400
+        self.screen_height = 700
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 5
+        self.ship_limit = 3
+
+        # Bullet settings
+        self.bullet_speed = 10
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 10
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
